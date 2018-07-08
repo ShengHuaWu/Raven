ae76a8fcc4a4f4b30901afc3ed8d9a4cbadef0a1
ShengHua Wu <fantasy0404@gmail.com>
2018-07-08 14:20:46 +0200

    Append remainders of CRUD method.

--- Sources/App/Controllers/UsersController.swift
+++ Sources/App/Controllers/UsersController.swift
@@ -3,15 +3,36 @@ import Vapor
 struct UsersController: RouteCollection {
     func boot(router: Router) throws {
         let usersRoute = router.grouped("api", "users")
+        usersRoute.post(use: createHandler)
         usersRoute.get(use: getAllHandler)
-        usersRoute.post(User.self, use: createHandler)
+        usersRoute.get(User.parameter, use: getHandler)
+        usersRoute.put(User.parameter, use: updateHandler)
+        usersRoute.delete(User.parameter, use: deleteHandler)
     }
     
-    func createHandler(_ req: Request, user: User) throws -> Future<User> {
-        return user.save(on: req)
+    func createHandler(_ req: Request) throws -> Future<User> {
+        // `req.content.decode(User.self)` means obtain the user from JSON body
+        return try req.content.decode(User.self).save(on: req)
     }
     
     func getAllHandler(_ req: Request) throws -> Future<[User]> {
         return User.query(on: req).all()
+    }
+    
+    func getHandler(_ req: Request) throws -> Future<User> {
+        // `req.parameters.next(User.self)` means fetching the user with id from db
+        return try req.parameters.next(User.self)
+    }
+    
+    func updateHandler(_ req: Request) throws -> Future<User> {
+        return try flatMap(to: User.self, req.parameters.next(User.self), req.content.decode(User.self)) { (user, updatedUser) in
+            user.name = updatedUser.name
+            user.username = updatedUser.username
+            return user.save(on: req)
+        }
+    }
+    
+    func deleteHandler(_ req: Request) throws -> Future<HTTPStatus> {
+        return try req.parameters.next(User.self).delete(on: req).transform(to: HTTPStatus.noContent)
     }
 }
